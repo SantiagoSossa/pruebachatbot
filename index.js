b0b713b64057f8ac2647f6314623e5632bcde975
SantiagoSossa <santiagosossa95@gmail.com>
2018-01-29 15:10:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -27,6 +27,20 @@ restService.post("/echo", function(req, res) {
   });
 });
 
+restService.post(msg, function(req, res) {
+  var speech =
+    req.body.result &&
+    req.body.result.parameters &&
+    req.body.result.parameters.prueba
+      ? "hola hello"
+      : "Seems like some problem. Speak again.";
+  return res.json({
+    speech: speech,
+    displayText: speech,
+    source: "webhook-echo-sample"
+  });
+});
+
 restService.post("/audio", function(req, res) {
   var speech = "";
   switch (req.body.result.parameters.AudioSample.toLowerCase()) {
