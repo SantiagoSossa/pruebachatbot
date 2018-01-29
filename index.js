48f1ca552dede3ac51034b36a904c5682b5b59a6
SantiagoSossa <santiagosossa95@gmail.com>
2018-01-29 15:25:26 -0500

    Update index.js

--- index.js
+++ index.js
@@ -27,19 +27,16 @@ restService.use(bodyParser.json());
   });
 });
 */
-restService.post("/echo", function(req, res) {
-  var speech =
-    req.body.result &&
-    req.body.result.parameters &&
-    req.body.result.parameters.prueba
-      ? req.body.result.parameters.echoText
-      : "Seems like some problem. Speak again.";
-  speech = "prueba"
-  return res.json({
-    speech: speech,
-    displayText: speech,
-    source: "webhook-echo-sample"
-  });
+restService.post(msg, function(req, res) {
+  
+if (msg.text.toString().toLowerCase().indexOf("men") === 0) {
+speech="hola desde el codigo";
+return res.json({
+speech: speech,
+displayText: speech,
+source: "webhook-echo-sample"
+});
+} 
 });
 
 restService.post("/audio", function(req, res) {
