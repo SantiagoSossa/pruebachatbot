9858d8c25f8f9ed0312ad0cf81ca93a7aa3c8953
SantiagoSossa <santiagosossa95@gmail.com>
2018-02-02 09:31:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,6 @@ restService.post("", function(req, res) {
   }
   else if(req.body.result && req.body.result.parameters && req.body.result.parameters.prueba)
   {
-    //speech = req.body.result.parameters.prueba
     speech = "Hola " + req.body.result.parameters.prueba
   }
   else if(req.body.result && req.body.result.parameters && req.body.result.parameters.numero)
